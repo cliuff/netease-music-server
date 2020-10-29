04c140c824a929ad04355f5719688c2dd01d1349
cliuff <35420834+cliuff@users.noreply.github.com>
2020-10-29 18:07:01 +0800

    Spring Initializr base

---
+++ src/main/java/cn/edu/cqut/cat/se/nemu/NeteaseMusicServerApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cqut.cat.se.nemu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeteaseMusicServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeteaseMusicServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/cqut/cat/se/nemu/NeteaseMusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.cqut.cat.se.nemu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeteaseMusicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
