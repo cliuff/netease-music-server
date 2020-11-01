0f7544b75084175a78f5bd8f503454d620f9999c
cliuff <35420834+cliuff@users.noreply.github.com>
2020-11-01 15:28:13 +0800

    歌单Demo

--- src/main/java/cn/edu/cqut/cat/se/nemu/controller/PlaylistController.java
+++ src/main/java/cn/edu/cqut/cat/se/nemu/controller/PlaylistController.java
@@ -1,21 +1,39 @@
 package cn.edu.cqut.cat.se.nemu.controller;
 
 
+import cn.edu.cqut.cat.se.nemu.entity.Playlist;
+import cn.edu.cqut.cat.se.nemu.result.DataResponse;
+import cn.edu.cqut.cat.se.nemu.service.IPlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author CQUT CAT SE 2020
  * @since 2020-11-01
  */
-@Controller
+@RestController
 @RequestMapping("/playlist")
 public class PlaylistController {
 
+    private final IPlaylistService service;
+
+    @Autowired
+    public PlaylistController(IPlaylistService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public DataResponse getPlaylists() {
+        List<Playlist> data = service.list();
+        return new DataResponse().setDataList(data);
+    }
 }
 
