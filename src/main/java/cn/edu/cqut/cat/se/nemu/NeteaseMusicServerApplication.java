03dfef869e10f5c4311bfbe99713a8c64edb3d71
cliuff <35420834+cliuff@users.noreply.github.com>
2020-11-01 15:24:13 +0800

    修复

--- src/main/java/cn/edu/cqut/cat/se/nemu/NeteaseMusicServerApplication.java
+++ src/main/java/cn/edu/cqut/cat/se/nemu/NeteaseMusicServerApplication.java
@@ -1,8 +1,10 @@
 package cn.edu.cqut.cat.se.nemu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cn.edu.cqut.cat.se.nemu.mapper")
 @SpringBootApplication
 public class NeteaseMusicServerApplication {
 
